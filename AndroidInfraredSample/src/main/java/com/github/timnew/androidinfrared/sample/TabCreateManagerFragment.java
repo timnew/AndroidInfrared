15731d743b7696e6501af818144920672a138639
DD <timnew.wti@gmail.com>
2013-12-13 00:59:11 +0800

    Clean unused code

--- AndroidInfraredSample/src/main/java/com/github/timnew/androidinfrared/sample/TabCreateManagerFragment.java
+++ AndroidInfraredSample/src/main/java/com/github/timnew/androidinfrared/sample/TabCreateManagerFragment.java
@@ -1,19 +1,15 @@
 package com.github.timnew.androidinfrared.sample;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 import com.github.timnew.androidinfrared.IrdaManager;
-import com.github.timnew.androidinfrared.IrdaProtocols;
 import com.github.timnew.androidinfrared.R;
-import com.github.timnew.androidinfrared.RawIrSequence;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EFragment;
 
 import static com.github.timnew.androidinfrared.IrdaProtocols.NEC.buildNEC;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 @EFragment(R.layout.tab_create_manager)
 public class TabCreateManagerFragment extends Fragment {
 
@@ -38,11 +34,6 @@ public class TabCreateManagerFragment extends Fragment {
     @Click(R.id.off_button)
     public void sendOff() {
         irdaManager.sendSequence(OFF_SEQENCE);
-    }
-
-    @Click(R.id.parse_button)
-    public void sendRaw() {
-//        irdaManager.sendSequence(RawIrSequence.parseProntoCode("0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0696 0152 0055 0015 0e23"));
     }
 }
 
